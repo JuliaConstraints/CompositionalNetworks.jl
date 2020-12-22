ce608409fa5493a1c1525642e4bfa72524f1fb9c
Documenter.jl <documenter@juliadocs.github.io>
2020-12-22 02:05:53 +0000

    build based on fecd396

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = CompositionalNetworks","category":"page"},{"location":"#Compositional-Networks","page":"Home","title":"Compositional Networks","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [CompositionalNetworks]","category":"page"}]
+}
