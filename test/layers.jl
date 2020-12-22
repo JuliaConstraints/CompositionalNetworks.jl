e296b56785690bbfc1b2b2262eaf54745c2d8d16
Azzaare <jf@baffier.fr>
2020-12-22 16:53:55 +0900

    Added ref ICN = CompositionalNetworks in tests

--- test/layers.jl
+++ test/layers.jl
@@ -4,7 +4,7 @@
 v1 = [1, 5, 2, 4, 3]
 v2 = [1, 2, 3, 2, 1]
 
-@test _identity(v1) == v1
-@test _identity(v2) == v2
+@test ICN._identity(v1) == v1
+@test ICN._identity(v2) == v2
 
 # @test _count_eq(v) == 
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,8 @@
 using CompositionalNetworks
 using Test
 
+ICN = CompositionalNetworks
+
 @testset "CompositionalNetworks.jl" begin
     include("layers.jl")
 end
