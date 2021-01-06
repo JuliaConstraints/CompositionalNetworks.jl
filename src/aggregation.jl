1e4d889d0e4a9f5f0587d3a0d6bcc2c1063e5575
Jean-Francois Baffier <jf@baffier.fr>
2021-01-06 14:13:15 +0900

    tentative fix (#13)
    * tentative fix
    
    * Renamed sum into ar_sum, ag_sum ...
    
    * Renamed in comparison.jl
    
    * Untyping operations to handle x86

--- src/aggregation.jl
+++ src/aggregation.jl
@@ -2,13 +2,13 @@
     _sum(x::V)
 Aggregate through `+` a vector into a single scalar.
 """
-_sum(x::V) where {T <: Number, V <: AbstractVector{T}} = reduce(+, x)
+_ag_sum(x) = reduce(+, x)
 
 """
     _count_positive(x::V)
 Count the number of strictly positive elements of `x`.
 """
-_count_positive(x::V) where {T <: Number, V <: AbstractVector{T}} = count(y -> y > 0.0, x)
+_ag_count_positive(x) = count(y -> y > 0.0, x)
 
 """
     aggregation_layer()
@@ -16,8 +16,8 @@ Generate the layer of aggregation functions of the ICN.
 """
 function aggregation_layer()
     aggregations = LittleDict{Symbol, Function}(
-        :sum => _sum,
-        :count_positive => _count_positive,
+        :sum => _ag_sum,
+        :count_positive => _ag_count_positive,
     )
 
     return Layer(aggregations, true)
--- src/comparison.jl
+++ src/comparison.jl
@@ -1,51 +1,41 @@
 # doc in transformation.jl
-_identity(x::T) where T <: Number = identity(x)
+_co_identity(x) = identity(x)
 
 """
     _abs_diff_val_param(x::T, param::T)
 Return the absolute difference between `x` and `param`.
 """
-_abs_diff_val_param(x::T, param::T) where T <: Number = abs(x - param)
+_co_abs_diff_val_param(x, param) = abs(x - param)
 
 """
     _val_minus_param(x::T, param::T)
     _param_minus_val(x::T, param::T)
 Return the difference `x - param` (resp. `param - x`) if positive, `0.0` otherwise.
 """
-_val_minus_param(x::T, param::T) where T <: Number = max(0.0, x - param)
-_param_minus_val(x::T, param::T) where T <: Number = max(0.0, param - x)
+_co_val_minus_param(x, param) = max(0.0, x - param)
+_co_param_minus_val(x, param) = max(0.0, param - x)
 
 """
     _euclidian_param(x::T, param::T, dom_size::T2)
     _euclidian(x::T, dom_size::T2)
 Compute an euclidian norm , possibly weigthed by `param`, on a scalar.
 """
-function _euclidian_param(x::T, param::T, dom_size::T2) where {T <: Number, T2 <: Number}
-    return x == param ? 0.0 : (1.0 + abs(x - param) \ dom_size)
-end
-function _euclidian(x::T, dom_size::T2) where {T <: Number, T2 <: Number}
-    return _euclidian_param(x, zero(T), dom_size)
-end
+_co_euclidian_param(x, param, d_size) = x == param ? 0.0 : (1.0 + abs(x - param) \ d_size)
+_co_euclidian(x, d_size) = _co_euclidian_param(x, 0.0, d_size)
 
 """
     _abs_diff_val_vars(x::T, nvars::Int)
 Return the absolute difference between `x` and the number of variables.
 """
-function _abs_diff_val_vars(x::T, nvars::Int) where {T <: Number}
-    return abs(x - nvars)
-end
+_co_abs_diff_val_vars(x, nvars) = abs(x - nvars)
 
 """
     _val_minus_vars(x::T, nvars::Int)
     _vars_minus_val(x::T, nvars::Int)
 Return the difference `x - nvars` (resp. `nvars - x`) if positive, `0.0` otherwise.
 """
-function _val_minus_vars(x::T, nvars::Int) where {T <: Number}
-    return _val_minus_param(x, nvars)
-end
-function _vars_minus_val(x::T, nvars::Int) where {T <: Number}
-    return _param_minus_val(x, nvars)
-end
+_co_val_minus_vars(x, nvars) = _co_val_minus_param(x, nvars)
+_co_vars_minus_val(x, nvars) = _co_param_minus_val(x, nvars)
 
 """
     comparison_layer(nvars, dom_size, param = nothing)
@@ -53,19 +43,19 @@ Generate the layer of transformations functions of the ICN. Iff `param` value is
 """
 function comparison_layer(nvars, dom_size, param = nothing)
     comparisons = LittleDict{Symbol, Function}(
-        :identity => _identity,
-        :euclidian => (x -> _euclidian(x, dom_size)),
-        :abs_diff_val_vars => (x -> _abs_diff_val_vars(x, nvars)),
-        :val_minus_vars => (x -> _val_minus_vars(x, nvars)),
-        :vars_minus_val => (x -> _vars_minus_val(x, nvars)),
+        :identity => _co_identity,
+        :euclidian => (x -> _co_euclidian(x, dom_size)),
+        :abs_diff_val_vars => (x -> _co_abs_diff_val_vars(x, nvars)),
+        :val_minus_vars => (x -> _co_val_minus_vars(x, nvars)),
+        :vars_minus_val => (x -> _co_vars_minus_val(x, nvars)),
     )
 
     if !isnothing(param)
         comparisons_param = LittleDict{Symbol, Function}(
-            :abs_diff_val_param => (x -> _abs_diff_val_param(x, param)),
-            :val_minus_param => (x -> _val_minus_param(x, param)),
-            :param_minus_val => (x -> _param_minus_val(x, param)),
-            :euclidian_param => (x -> _euclidian_param(x, param, dom_size)),
+            :abs_diff_val_param => (x -> _co_abs_diff_val_param(x, param)),
+            :val_minus_param => (x -> _co_val_minus_param(x, param)),
+            :param_minus_val => (x -> _co_param_minus_val(x, param)),
+            :euclidian_param => (x -> _co_euclidian_param(x, param, dom_size)),
         )
         comparisons = LittleDict{Symbol, Function}(union(comparisons, comparisons_param))
     end
