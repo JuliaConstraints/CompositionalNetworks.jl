c6a39e6a2d51a8b57d02f76308ed2d98c41dd80e
Azzaare <jf@baffier.fr>
2021-01-12 14:40:31 +0900

    Updated learning options

--- src/learn.jl
+++ src/learn.jl
@@ -39,26 +39,26 @@ Create an ICN, optimize it, and return its composition.
 """
 function learn_compose(X, X_sols; nvars, dom_size, param=nothing,
     global_iter=10, local_iter=100, metric=hamming, popSize=200,
-    action = :composition
+    action=:composition
 )
     icn = ICN(nvars=nvars, dom_size=dom_size, param=param)
     optimize!(icn, X, X_sols, global_iter, local_iter; metric=metric, popSize=200)
     @info show_composition(icn)
 
-    return compose(icn, action = action)
+    return compose(icn, action=action)
 end
 
 function explore_learn_compose(concept; domains, param=nothing,
     search=:complete, global_iter=10, local_iter=100, metric=hamming, popSize=200,
-    action = :composition,
+    action=:composition,
 )
     if search == :complete
         X_sols, X = _complete_search_space(domains, concept)
         union!(X, X_sols)
         return learn_compose(X, X_sols;
             nvars=length(domains), dom_size=maximum(_length, domains),
-            local_iter = local_iter, global_iter = global_iter, param = param,
-            action = action)
+            local_iter=local_iter, global_iter=global_iter, param=param,
+            action=action)
     end
 end
 
@@ -76,9 +76,24 @@ function _compose_to_string(symbols, name)
     return julia_string
 end
 
-function compose_to_file!(icn, name, path, language = :Julia)
+function compose_to_file!(icn::ICN, name, path, language=:Julia)
     language == :Julia # TODO: handle other languages
-    file = open(path,"w")
-    write(file, _compose_to_string(compose(icn, action = :symbols), name))
+    file = open(path, "w")
+    write(file, _compose_to_string(compose(icn, action=:symbols), name))
+    close(file)
+end
+
+function compose_to_file!(concept, name, path;
+    domains, param=nothing, language=:Julia,
+    search=:complete, global_iter=10, local_iter=100, metric=hamming, popSize=200
+)
+    language == :Julia # TODO: handle other languages
+    
+    symbols = explore_learn_compose(concept, domains=domains, param=param, search=search,
+        global_iter=global_iter, local_iter=local_iter , metric=metric, popSize=popSize,
+        action=:symbols)
+
+    file = open(path, "w")
+    write(file, _compose_to_string(symbols, name))
     close(file)
 end
