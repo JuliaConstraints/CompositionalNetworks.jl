a12fa526b059f0b96aed40a233910ff2ff172ae8
Jean-Francois Baffier <jf@baffier.fr>
2021-01-14 23:30:33 +0900

    Doc for beta announcement (#21)
    * First usable doc
    
    * Updated README.md
    
    * bump version

--- src/aggregation.jl
+++ src/aggregation.jl
@@ -12,7 +12,7 @@ _ag_count_positive(x) = count(y -> y > 0.0, x)
 
 """
     aggregation_layer()
-Generate the layer of aggregation functions of the ICN.
+Generate the layer of aggregations of the ICN. The operations are mutually exclusive, that is only one will be selected.
 """
 function aggregation_layer()
     aggregations = LittleDict{Symbol, Function}(
--- src/arithmetic.jl
+++ src/arithmetic.jl
@@ -1,14 +1,18 @@
 """
     _ar_sum(x)
-    _ar_prod(x)
-Reduce `k = length(x)` vectors through sum/product to a single vector.
+Reduce `k = length(x)` vectors through sum to a single vector.
 """
 _ar_sum(x) = reduce((y, z) -> y .+ z, x)
+
+"""
+    _ar_prod(x)
+Reduce `k = length(x)` vectors through product to a single vector.
+"""
 _ar_prod(x) = reduce((y, z) -> y .* z, x)
 
 """
     arithmetic_layer()
-Generate the layer of arithmetic functions of the ICN.
+Generate the layer of arithmetic operations of the ICN. The operations are mutually exclusive, that is only one will be selected.
 """
 function arithmetic_layer()
     arithmetics = LittleDict{Symbol, Function}(
