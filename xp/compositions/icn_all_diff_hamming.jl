f0e4808cbef0e6ea75520b6170f8e428087c33bd
Khalil Chrit <ch.khalil21@gmail.com>
2021-09-04 14:19:26 +0100

    .

--- xp/compositions/icn_all_diff_hamming.jl
+++ xp/compositions/icn_all_diff_hamming.jl
@@ -5,3 +5,7 @@ function icn_all_diff_hamming(x; X = zeros(length(x), 1), param=nothing, dom_siz
     end
     return CompositionalNetworks.ag_count_positive(@view X[:, 1]) |> (y -> CompositionalNetworks.co_identity(y; param, dom_size, nvars=length(x)))
 end
+#Generation:
+ #8.348 s (53084802 allocations: 2.95 GiB)
+#Use:
+# 1.052 μs (6 allocations: 352.0 bytes)
