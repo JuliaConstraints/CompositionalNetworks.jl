60ed9cf2e6276882095959b0f9ecdefc7629e6f8
Khalil Chrit <ch.khalil21@gmail.com>
2021-09-06 13:43:11 +0100

    ..

--- src/metrics.jl
+++ src/metrics.jl
@@ -7,7 +7,7 @@ hamming(x, X) = mapreduce(y -> sum(x .!= y), min, X; init = length(x))
 """
     minkowski(x, X, p)
 """
-minkowski(x, X, p) = mapreduce(y -> sum(abs(x .- y)^p)^(1/p), min, X; init = sum(x.^p)^(1/p))
+minkowski(x, X, p) = mapreduce(y -> (sum(abs.(x .- y).^p))^(1/p), min, X; init = sum(x.^p)^(1/p))
 
 """
     manhattan(x, X)
