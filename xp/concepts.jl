8af2d6e54e78d2778cc255f412561e887917d65b
Khalil Chrit <ch.khalil21@gmail.com>
2021-09-07 00:05:02 +0100

    added nooverlap and sumequalparam

--- xp/concepts.jl
+++ xp/concepts.jl
@@ -21,9 +21,22 @@ concept_less_than_param(x, param) = x[1] ≤ param
 #ordered
 concept_ordered(x) = issorted(x)
 
-#no_overlap
+#no_overlap (general idea, still need to review it)
+function concept_no_overlap(x, l) 
+    for i in 1:length(x)
+        for j in i+1:length(x)-1
+            if (x[i]+l[i] > x[j] && x[j]+l[j] > x[i])
+                return false
+            end
+        end
+    end
+    return true
+end
+
 
 #sum of vars = c
+concept_sum_equal_param(x; param) = sum(x) == param
+
 
 #concept_list = [concept_all_diff]
 concept_list = [concept_all_diff, concept_dist_diff, concept_all_eq, concept_less_than_param, concept_ordered]
