b231059c1d24f8ce3a2d639287fa59f14ace31e8
azzaare <jf@baffier.fr>
2021-09-07 16:16:39 +0900

    Delete misplaced file

--- icn_all_diff.jl
+++
@@ -1,7 +0,0 @@
-function icn_all_diff(x; X = zeros(length(x), 1), param=nothing, dom_size)
-    CompositionalNetworks.tr_in(Tuple([CompositionalNetworks.tr_count_eq_left]), X, x, param)
-    for i in 1:length(x)
-        X[i,1] = CompositionalNetworks.ar_sum(@view X[i,:])
-    end
-    return CompositionalNetworks.ag_count_positive(@view X[:, 1]) |> (y -> CompositionalNetworks.co_identity(y; param, dom_size, nvars=length(x)))
-end
